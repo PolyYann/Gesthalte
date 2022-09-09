0eacc85a012f19d1fa6f4e29e8ad373d733b03f5
Yann <y.lebeau84@gmail.com>
2022-09-09 10:16:29 -0400

    menu in main

--- app/src/main/java/com/lebeau/gesthalte/MainActivity.java
+++ app/src/main/java/com/lebeau/gesthalte/MainActivity.java
@@ -3,12 +3,14 @@ package com.lebeau.gesthalte;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent monIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +30,28 @@ public class MainActivity extends AppCompatActivity {
             case R.id.app_bar_search:
                 break;
             case R.id.registre:
+                monIntent = new Intent(MainActivity.this, RegistreActivity.class);
+                startActivity(monIntent);
                 break;
             case R.id.modifier:
+                monIntent = new Intent(MainActivity.this, ModifierActivity.class);
+                startActivity(monIntent);
                 break;
             case R.id.ajouter:
+                monIntent = new Intent(MainActivity.this, AjouterActivity.class);
+                startActivity(monIntent);
                 break;
             case R.id.presences:
+                monIntent = new Intent(MainActivity.this, PresencesActivity.class);
+                startActivity(monIntent);
                 break;
             case R.id.locaux:
+                Toast.makeText(this, "Gestion des locaux en developpement",
+                        Toast.LENGTH_LONG).show();
                 break;
             case R.id.horraires:
+                Toast.makeText(this, "Gestion des horraires en developpement",
+                        Toast.LENGTH_LONG).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
