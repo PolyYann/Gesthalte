b1062b20b0839795cb6ad7058ba25e859db6f760
Yann <y.lebeau84@gmail.com>
2022-09-09 10:01:17 -0400

    menu + activities

---
+++ app/src/main/java/com/lebeau/gesthalte/AjouterActivity.java
@@ -0,0 +1,14 @@
+package com.lebeau.gesthalte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjouterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter);
+    }
+}
--- app/src/main/java/com/lebeau/gesthalte/MainActivity.java
+++ app/src/main/java/com/lebeau/gesthalte/MainActivity.java
@@ -1,8 +1,11 @@
 package com.lebeau.gesthalte;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int option = item.getItemId();
+        switch (option) {
+            case R.id.app_bar_search:
+                break;
+            case R.id.registre:
+                break;
+            case R.id.modifier:
+                break;
+            case R.id.ajouter:
+                break;
+            case R.id.presences:
+                break;
+            case R.id.locaux:
+                break;
+            case R.id.horraires:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
