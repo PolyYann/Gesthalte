86b7685bef277540abd1f3e123e237e6e4152ec8
Yann <y.lebeau84@gmail.com>
2022-09-12 15:18:10 -0400

    1

--- app/src/main/java/com/lebeau/gesthalte/PresencesActivity.java
+++ app/src/main/java/com/lebeau/gesthalte/PresencesActivity.java
@@ -9,6 +9,6 @@ public class PresencesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_presences);
+
     }
 }
