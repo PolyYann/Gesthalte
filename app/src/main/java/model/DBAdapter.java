eef297b467f1cab64683c787e87c7bf50a12eb03
Yann <y.lebeau84@gmail.com>
2022-09-29 10:07:30 -0400

    modifier enfant a tester

--- app/src/main/java/model/DBAdapter.java
+++ app/src/main/java/model/DBAdapter.java
@@ -29,7 +29,8 @@ public class DBAdapter {
 
     public void ajouterEnfant(Enfant enf){
         openBD();
-
+        registreEnfants = RegistreEnfants.getInstance();
+        registreEnfants.ajouterEnfant(enf);
         ContentValues cv = new ContentValues();
         cv.put(COL_NOM, enf.getNom());
         cv.put(COL_PRENOM, enf.getPrenom());
@@ -87,42 +88,5 @@ public class DBAdapter {
         closeBD();
     }
 
-    public void afficherEnfants(){
-        openBD();
-        //indiquer les colonnes du sélect
-        String[] colonnes = {COL_ID, COL_NOM, COL_PRENOM, COL_DATE_NAISSANCE, COL_AGE, COL_TELEPHONE,
-                COL_ADRESSE, COL_VILLE, COL_PROVINCE, COL_CODE_POSTAL, COL_ALLERGIES, COL_PARENT1,
-                COL_PARENT2, COL_PARENT3, COL_PERS_AUTORISEES, COL_EST_PRESENT};
-        Cursor cursor = db.query(EnfantDBHelper.TABLE_E, colonnes, null, null, null, null, null);
-        //parcourir le curseur et remplir le registre mémoire
-        boolean estPresent;
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            int id = cursor.getInt(0);
-            String nom = cursor.getString(1);
-            String prenom = cursor.getString(2);
-            String dateNaissance = cursor.getString(3);
-            int age = cursor.getInt(4);
-            String telephone = cursor.getString(5);
-            String adresse = cursor.getString(6);
-            String ville = cursor.getString(7);
-            String province = cursor.getString(8);
-            String codePostal = cursor.getString(9);
-            String allergies = cursor.getString(10);
-            String parent1 = cursor.getString(11);
-            String parent2 = cursor.getString(12);
-            String parent3 = cursor.getString(13);
-            String persAutorisees = cursor.getString(14);
-            int present = cursor.getInt(15);
-            if(present==1){
-                estPresent = true;
-            }else {
-                estPresent = false;
-            }
-            Enfant enf = new Enfant(id, nom, prenom, dateNaissance, age, telephone, adresse, ville,
-                    province, codePostal, allergies, parent1, parent2, parent3, persAutorisees, estPresent);
-            registreEnfants.ajouterEnfant(enf);
-              cursor.moveToNext();
-        }
-    }
+
 }
--- app/src/main/java/model/RegistreEnfants.java
+++ app/src/main/java/model/RegistreEnfants.java
@@ -9,13 +9,20 @@ public class RegistreEnfants {
     private static final RegistreEnfants instance = new RegistreEnfants();
 
     private static final ArrayList<Enfant> listEnfants = new ArrayList<>();
-
+    private static final ArrayList<Enfant> mockEnfants = new ArrayList<>(List.of(new Enfant("Dupont", "Jean", "datenaissance",24,"telephone","addresse","ville","province","codePost","allergies", "parent1","parent2","parent3","persAuth",false),new Enfant("Dupont", "Jean", "datenaissance",24,"telephone","addresse","ville","province","codePost","allergies", "parent1","parent2","parent3","persAuth",false)
+            ,new Enfant("Dupont1", "Jean", "datenaissance",24,"telephone","addresse","ville","province","codePost","allergies", "parent1","parent2","parent3","persAuth",false),
+            new Enfant("Dupont2", "Jean", "datenaissance",24,"telephone","addresse","ville","province","codePost","allergies", "parent1","parent2","parent3","persAuth",false),
+            new Enfant("Dupont3", "Jean", "datenaissance",24,"telephone","addresse","ville","province","codePost","allergies", "parent1","parent2","parent3","persAuth",false),
+            new Enfant("Dupont4", "Jean", "datenaissance",24,"telephone","addresse","ville","province","codePost","allergies", "parent1","parent2","parent3","persAuth",false),
+            new Enfant("Dupont5", "Jean", "datenaissance",24,"telephone","addresse","ville","province","codePost","allergies", "parent1","parent2","parent3","persAuth",false)));
     private RegistreEnfants() {
+
         if (instance != null) {
             throw new IllegalStateException("Already instantiated");
         }
     }
     public static RegistreEnfants getInstance() {
+        listEnfants.addAll(mockEnfants);
         return instance;
     }
     public void ajouterEnfant(Enfant enfant) {
@@ -28,8 +35,6 @@ public class RegistreEnfants {
         return listEnfants.get(id);
     }
     public List<Enfant> getEnfants(Context context) {
-        DBAdapter dbAdapter = new DBAdapter(context);
-         dbAdapter.afficherEnfants();
         return listEnfants;
     }
 
